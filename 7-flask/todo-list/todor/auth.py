2af58b4fe9462b099e4546138858f178174de2f5
julian <julianmoreno263@gmail.com>
2026-05-04 13:13:33 -0500

    Creando templates de registro de usuarios y de login, renderizando estas vistas desde auth.py con render_template

--- 7-flask/todo-list/todor/auth.py
+++ 7-flask/todo-list/todor/auth.py
@@ -1,6 +1,6 @@
 #archivo para la autenticacion de usuario, aqui tambien utilizamos blueprint
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 #instancia de blueprint,este es el prefijo para las demas rutas
 bp=Blueprint("auth",__name__,url_prefix="/auth")
@@ -9,9 +9,9 @@ bp=Blueprint("auth",__name__,url_prefix="/auth")
 #ruta /list
 @bp.route("/register")
 def register():
-    return "Registrar usuario"
+    return render_template("auth/register.html")
 
 #ruta /create
 @bp.route("/login")
 def login():
-    return "Iniciar sesión"
+        return render_template("auth/login.html")
