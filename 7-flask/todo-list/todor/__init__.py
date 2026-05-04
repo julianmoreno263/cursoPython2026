1520ab270d339462a7f7a67e6042fa7ed479d61e
julian <julianmoreno263@gmail.com>
2026-05-04 15:10:02 -0500

    Creando modelos de base de datos utilizando el orm sqlalchemy y realizando pruebas con flask shell

--- 7-flask/todo-list/todor/__init__.py
+++ 7-flask/todo-list/todor/__init__.py
@@ -1,13 +1,14 @@
 #configuraciones iniciales de la app
 
 from flask import Flask, render_template
-from . import todo
-from . import auth
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 #creamos una funcion que crea la app, esta funcion nos permitira reutilizarla donde la necesitemos,por ejemplo  cuando necesitemos usar la bd con una instancia de la aplicacion podemos reutilizar esta funcion 
 
-
+#crear instancia de sqlalchemy
+db=SQLAlchemy()
 
 def create_app():
     #creamos app
@@ -15,8 +16,17 @@ def create_app():
     #configuracion del proyecto
     app.config.from_mapping(
         DEBUG=True,
-        SECRET_KEY="dev"
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI="sqlite:///todolist.db"
     )
+
+    #inicializamos la conexion a la db
+    db.init_app(app)
+
+    # --- MOVEMOS LAS IMPORTACIONES AQUÍ ABAJO ---
+    # Esto rompe el círculo: 'db' ya existe cuando estos archivos se cargan
+    from . import todo
+    from . import auth
 
     #registrar blueprint
     app.register_blueprint(todo.bp)
@@ -27,6 +37,11 @@ def create_app():
     @app.route("/")
     def index():
         return render_template("index.html")
+    
+
+    #con esto migramos el modelo de la db
+    with app.app_context():
+        db.create_all()
     
     return app
     
--- 7-flask/todo-list/todor/auth.py
+++ 7-flask/todo-list/todor/auth.py
@@ -1,6 +1,7 @@
 #archivo para la autenticacion de usuario, aqui tambien utilizamos blueprint
 
 from flask import Blueprint, render_template
+from . import models  #con esta linea se migran los modelos para la bd
 
 #instancia de blueprint,este es el prefijo para las demas rutas
 bp=Blueprint("auth",__name__,url_prefix="/auth")
