5f9cc7f9e35e9318e1c4c8ca6acfb37e44aaae7f
julian <julianmoreno263@gmail.com>
2026-08-06 14:33:27 -0500

    Personalizando el admin django

--- django/blogpersonal/blog/admin.py
+++ django/blogpersonal/blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+#admin.site.register(Post)
+
+
+#clase para personalizar el panel de administrador en la app blog,lo hacemos con un decorador
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=('id','image','title','desc','created')
+    list_display_links=('id','title')
+    list_filter=('created','update')
+    search_fields=('title','desc')
+
+    readonly_fields=('created','update')
--- django/blogpersonal/blog/models.py
+++ django/blogpersonal/blog/models.py
@@ -1,15 +1,21 @@
-from email.mime import image
 
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Post(models.Model):
-    image=models.ImageField()
-    title=models.CharField(max_length=100)
-    desc=models.TextField()
-    content=models.TextField()
-    created=models.DateTimeField(auto_now_add=True)
-    update=models.DateTimeField(auto_now=True)
+    image=models.ImageField(verbose_name='Imagen')
+    title=models.CharField(max_length=100,verbose_name='Título')
+    desc=models.TextField(verbose_name='Descripción')
+    content=RichTextField(verbose_name='Contenido')
+    created=models.DateTimeField(auto_now_add=True,verbose_name='Fecha Creación')
+    update=models.DateTimeField(auto_now=True,verbose_name='Fecha Actualización')
+
+    class Meta:
+        verbose_name='Publicación'
+        verbose_name_plural='Publicaciones'
+        ordering=['-created']
+
 
     def __str__(self):
         return self.title
